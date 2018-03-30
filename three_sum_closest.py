cdbe7590b15b5b2b7fa8ecd7d7735390b9888556
Zheng Qin <zq17@line16.doc.ic.ac.uk>
2018-03-30 16:45:16 +0100

    three_sum_closest

---
+++ three_sum_closest.py
@@ -0,0 +1,39 @@
+class Solution:
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        nums.sort()
+        distance = 100
+        closest = 0
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i+1]:
+                continue
+            else:
+                left, right = i + 1, len(nums) - 1
+                while right > left:
+                    three_sum = nums[i] + nums[left] + nums[right]
+                    
+                    if three_sum == target:
+                        return target
+
+                    elif three_sum > target:
+                        if three_sum - target < distance:
+                            distance = three_sum - target
+                            closest = three_sum
+                        while right > left and nums[right] == nums[right-1]:
+                            right -= 1
+                        right -= 1
+
+                    else:
+                        if target - three_sum > distance:
+                            distance = target - three_sum
+                            closest = three_sum
+                        while right > left and nums[left] == nums[left+1]:
+                            left += 1
+                        left += 1
+        return closest
