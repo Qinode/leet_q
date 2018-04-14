90828749acf2cb015724d4363713b5dcd33073ec
Zheng Qin <zq17@matrix15.doc.ic.ac.uk>
2018-04-14 12:31:23 +0100

    count bit

---
+++ counting_bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+
+        result = [0]
+        for i in range(1, num + 1):
+            result.append(result[i>>1] + (i&1))
+        return result
+        
