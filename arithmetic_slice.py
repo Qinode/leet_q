a2af11d2ab34184a553d5d57f5928afe24f0f108
Zheng Qin <zheng.qin29@outlook.com>
2018-04-15 14:06:21 +0100

    merge remote

---
+++ arithmetic_slice.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        if len(A) < 3:
+            return 0
+        else:
+            res = 0
+            dp = [[(False, 0) for _ in range(len(A))] for _ in range(len(A))]
+            for i in range(len(A)):
+                for j in range(i + 2, len(A)):
+                    if j - i == 2:
+                        if A[j] - A[j - 1] == A[j - 1] - A[j - 2]:
+                            dp[i][j] = (True, A[j] - A[j - 1])
+                            res += 1
+                    else:
+                        _, slice = dp[i][j - 1]
+                        if A[j] - A[j - 1] == slice:
+                            dp[i][j] = (True, slice)
+                            res += 1
+
+        return res
