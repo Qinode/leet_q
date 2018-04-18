c51f49639085209bee0206d6bcbb903258e5d767
Zheng Qin <zheng.qin29@outlook.com>
2018-04-18 11:42:19 +0100

    work break

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from maximum_length_of_pair_chain import Solution
+from word_break import Solution
 
 if __name__ == '__main__':
     solution = Solution()
 
-    r = solution.findLongestChain([[7,9],[4,5],[7,9],[-7,-1],[0,10],[3,10],[3,6],[2,3]])
+    r = solution.wordBreak("goalspecial", ["go","goal","goals","special"])
     print(r)
