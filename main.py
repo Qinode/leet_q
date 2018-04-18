c636853da25ec2dc164fe55733a1012b7e2ca5d6
Zheng Qin <zq17@matrix15.doc.ic.ac.uk>
2018-04-18 21:48:32 +0100

    unique path ii

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from unique_path import Solution
+from unique_path_ii import Solution
 
 if __name__ == '__main__':
     solution = Solution()
 
-    r = solution.uniquePaths(7, 3)
+    path = [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ]
+
+    r = solution.uniquePathsWithObstacles(path)
     print(r)
