7d474be1effabbd72372ca68e2e592669191904e
Zheng Qin <zheng.qin29@outlook.com>
2018-04-22 22:03:44 +0100

    longest incresing subsequence

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -5,25 +5,21 @@ class Solution:
         :rtype: int
         """
 
-        sort_nums = sorted(nums)
         if len(nums) == 0:
             return 0
         elif len(nums) == 1:
             return 1
         else:
-            dp = [[0 for _ in range(len(nums) + 1)] for _ in range(len(nums) + 1)]
+            dp = [1 for _ in range(len(nums))]
+            max_len = 1
 
-            for i in range(1, len(nums) + 1):
-                dp[i][0] = dp[0][i] = i
-
-            for i in range(1, len(nums) + 1):
-                for j in range(1, len(nums) + 1):
-                    if sort_nums[i - 1] == nums[j - 1]:
-                        dp[i][j] = dp[i - 1][j - 1]
-                    else:
-                        dp[i][j] = min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]) + 1
-
-            print(dp)
-            return len(nums) - dp[-1][-1]
+            for i in range(1, len(nums)):
+                max_val = 0
+                for j in range(i):
+                    if nums[i] > nums[j]:
+                        max_val = max(max_val, dp[j])
+                dp[i] = max_val + 1
+                max_len = max(max_len, dp[i])
+            return max_len
 
 
