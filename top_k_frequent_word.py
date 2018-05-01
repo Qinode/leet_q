5a8d7793573b458c577250437a44dea8d1e4deb0
Zheng Qin <zheng.qin29@outlook.com>
2018-05-01 17:19:19 +0100

    top k frequent word

---
+++ top_k_frequent_word.py
@@ -0,0 +1,9 @@
+import collections
+
+
+class Solution(object):
+    def topKFrequent(self, words, k):
+        count = collections.Counter(words)
+        candidates = list(count.keys())
+        candidates.sort(key = lambda w: (-count[w], w))
+        return candidates[:k]
