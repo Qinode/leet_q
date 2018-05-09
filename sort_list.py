a05f2b82e49c78b1f14e9efd61dbbe2b9635ffa9
Zheng Qin <zheng.qin29@outlook.com>
2018-05-10 00:49:56 +0100

    sort list

---
+++ sort_list.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        if head is None or head.next is None:
+            return head
+        else:
+            middle = self.find_middle(head)
+            right = middle.next
+            middle.next = None
+            return self.merge_sort(self.sortList(head), self.sortList(right))
+
+            return stack[-1]
+
+    def find_middle(self, linked_list):
+        fast, slow = linked_list, linked_list
+        while fast and fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+    def merge_sort(self, l1, l2):
+        h = h_copy = ListNode(None)
+        while l1 is not None and l2 is not None:
+            if l1.val >= l2.val:
+                h_copy.next = l2
+                l2 = l2.next
+            else:
+                h_copy.next = l1
+                l1 = l1.next
+            h_copy = h_copy.next
+
+        h_copy.next = l1 or l2
+        return h.next
