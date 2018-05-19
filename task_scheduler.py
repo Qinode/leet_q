1ea8d2e01ab6ada9da6e91ad36c42785d4d35a9b
Zheng Qin <zq17@matrix14.doc.ic.ac.uk>
2018-05-19 11:10:45 +0100

    task scheduler

---
+++ task_scheduler.py
@@ -0,0 +1,35 @@
+import heapq
+
+
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+
+        n += 1
+        ans = 0
+
+        d = collections.Counter(tasks)
+        heap = [-c for c in d.values()]
+
+        heapq.heapify(heap)
+
+        while heap:
+            stack = []
+            cnt = 0
+            for _ in range(n):
+                if heap:
+                    c = heapq.heappop(heap)
+                    cnt += 1
+                    if c < -1:
+                        stack.append(c + 1)
+
+            for item in stack:
+                heapq.heappush(heap, item)
+
+            ans += n if heap else cnt
+
+        return ans
